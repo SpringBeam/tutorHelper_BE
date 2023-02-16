7979182d9ceccd55a7d01a857bb3d3dd02551f6c
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2023-02-16 16:52:54 +0900

    properties fixed

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+nohup java -jar -Dserver.port=$_{TARGET_PORT} /home/ec2-user/susukgwan/build/libs/* > /home/ec2-user/nohup.out 2>&1 &
+echo "New WAS runs at ${TARGET_PORT}."
+exit 0
