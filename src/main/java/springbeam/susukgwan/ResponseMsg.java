ea4cd709aefdbf820d5d0386fdb2290e92dece13
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2023-03-26 18:32:13 +0900

    response design for all api

---
+++ src/main/java/springbeam/susukgwan/ResponseMsg.java
@@ -0,0 +1,12 @@
+package springbeam.susukgwan;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ResponseMsg {
+    private String msg;
+}
--- src/main/java/springbeam/susukgwan/auth/AuthController.java
+++ src/main/java/springbeam/susukgwan/auth/AuthController.java
@@ -2,6 +2,7 @@ package springbeam.susukgwan.auth;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springbeam.susukgwan.auth.dto.RefreshDTO;
 import springbeam.susukgwan.auth.vo.TokenRefreshVO;
@@ -12,10 +13,8 @@ public class AuthController {
     @Autowired
     private AuthService authService;
     @PostMapping("/refresh")
-    public TokenRefreshVO refreshToken(HttpServletRequest request, @RequestBody RefreshDTO refreshDTO) {
+    public ResponseEntity refreshToken(HttpServletRequest request, @RequestBody RefreshDTO refreshDTO) {
         String accessToken = JwtHeaderUtil.getAccessToken(request);
-        TokenRefreshVO tokenRefreshVO = authService.refreshToken(accessToken, refreshDTO.getRefreshToken());
-        // TODO 응답코드, 응답 규격화하기
-        return tokenRefreshVO;
+        return authService.refreshToken(accessToken, refreshDTO.getRefreshToken());
     }
 }
