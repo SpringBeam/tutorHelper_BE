6cd347ffc6150e0e1d729f7e6a83b75d4e283825
kjy1101 <amelia1023@naver.com>
2023-04-10 20:05:00 +0900

    handle typemismatch exceptions

--- src/main/java/springbeam/susukgwan/ApiControllerAdvice.java
+++ src/main/java/springbeam/susukgwan/ApiControllerAdvice.java
@@ -3,6 +3,7 @@ package springbeam.susukgwan;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @RestControllerAdvice
 public class ApiControllerAdvice {
@@ -22,6 +25,7 @@ public class ApiControllerAdvice {
         this.messageSource = messageSource;
     }
 
+    /* Validation 예외처리 */
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
@@ -30,6 +34,26 @@ public class ApiControllerAdvice {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    /* TypeMismatch 예외처리 */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String, String>> handleTypeMismatchExceptions(HttpMessageNotReadableException ex){
+        Map<String, String> errors = new HashMap<>();
+
+        Pattern errorFieldPattern = Pattern.compile("\\[[\"](.*?)[\"]\\]");
+        Matcher errorFieldMatcher = errorFieldPattern.matcher(ex.getCause().getMessage());
+        String errorField = errorFieldMatcher.find() ? errorFieldMatcher.group(1) : "FAIL";
+
+        Pattern rightTypePattern = Pattern.compile("[`](.*?)[`]");
+        Matcher rightTypeMatcher = rightTypePattern.matcher(ex.getMessage());
+        String rightType = rightTypeMatcher.find() ? rightTypeMatcher.group(1) : "?";
+
+        String errorMessage = messageSource.getMessage("typeMismatch", new Object[] {rightType}, Locale.KOREA);
+        errors.put(errorField, errorMessage);
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    /* errors.properties 에서 커스텀 에러메세지 가져옴 */
     private String getErrorMessage(ObjectError error) {
         String[] codes = error.getCodes();
         for (String code : codes) {
