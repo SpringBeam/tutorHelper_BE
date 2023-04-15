b3b0a2dd9380e371fcc3e2f51f3bd2cc6e56f01d
kjy1101 <amelia1023@naver.com>
2023-04-15 14:17:00 +0900

    add authorization - review update & delete & check

--- src/main/java/springbeam/susukgwan/WebMvcConfig.java
+++ src/main/java/springbeam/susukgwan/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springbeam.susukgwan.review.ReviewAuthInterceptor;
 import springbeam.susukgwan.tag.TagAuthInterceptor;
 
 @Configuration
@@ -11,11 +12,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     TagAuthInterceptor tagAuthInterceptor;
 
+    @Autowired
+    ReviewAuthInterceptor reviewAuthInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tagAuthInterceptor)
                 .excludePathPatterns("/api/tag", "/api/tag/list") // 인터셉터가 가로채지 않는 url
                 .addPathPatterns("/api/tag/**"); // 인터셉터가 가로채는 url (authorization 확인이 필요한 api)
+        registry.addInterceptor(reviewAuthInterceptor)
+                .excludePathPatterns("/api/review", "api/review/list")
+                .addPathPatterns("/api/review/**");
     }
 
 }
---
+++ src/main/java/springbeam/susukgwan/review/ReviewAuthInterceptor.java
@@ -0,0 +1,52 @@
+package springbeam.susukgwan.review;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.HandlerMapping;
+import springbeam.susukgwan.ResponseMsg;
+import springbeam.susukgwan.ResponseMsgList;
+
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class ReviewAuthInterceptor implements HandlerInterceptor {
+
+    @Autowired
+    private final ReviewRepository reviewRepository;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        /* Review Update & Delete & Check Authorization */
+        String tutorIdStr = SecurityContextHolder.getContext().getAuthentication().getName();
+        Long tutorId = Long.parseLong(tutorIdStr);
+
+        Map<?, ?> pathVariables = (Map<?, ?>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+        Long reviewId = Long.parseLong((String)pathVariables.get("reviewId"));
+
+        Long tutorIdOfReview = reviewRepository.GetTutorIdOfReview(reviewId);
+
+        if (tutorIdOfReview != null && tutorId != tutorIdOfReview) {
+            String result = objectMapper.writeValueAsString(new ResponseMsg(ResponseMsgList.NOT_AUTHORIZED.getMsg()));
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            response.setStatus(403); // Forbidden
+            response.getWriter().write(result);
+
+            log.info("status code={}, body={}", response.getStatus(), ResponseMsgList.NOT_AUTHORIZED.getMsg());
+
+            return false;
+        }
+
+        return true;
+    }
+}
