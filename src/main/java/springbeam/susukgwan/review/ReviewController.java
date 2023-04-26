28eaf0cbee23d152b3200bc052da7d726fd73390
kjyeon1101 <kjyeon1101@gmail.com>
2023-04-26 16:22:47 +0900

    delete log

--- src/main/java/springbeam/susukgwan/review/ReviewController.java
+++ src/main/java/springbeam/susukgwan/review/ReviewController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import springbeam.susukgwan.Logging;
 import springbeam.susukgwan.review.dto.ReviewRequestDTO;
 
 @RestController
@@ -15,26 +14,26 @@ public class ReviewController {
 
     @PostMapping("")
     public ResponseEntity<?> createReview (@Valid @RequestBody ReviewRequestDTO.Create createReview){
-        return Logging.PrintLogAndReturnResponseEntity(reviewService.createReview(createReview));
+        return reviewService.createReview(createReview);
     }
 
     @PutMapping("/{reviewId}")
     public ResponseEntity<?> updateReview (@PathVariable("reviewId") Long reviewId, @RequestBody ReviewRequestDTO.Update updateReview) {
-        return Logging.PrintLogAndReturnResponseEntity(reviewService.updateReview(reviewId, updateReview));
+        return reviewService.updateReview(reviewId, updateReview);
     }
 
     @DeleteMapping("/{reviewId}")
     public ResponseEntity<?> deleteReview (@PathVariable("reviewId") Long reviewId) {
-        return Logging.PrintLogAndReturnResponseEntity(reviewService.deleteReview(reviewId));
+        return reviewService.deleteReview(reviewId);
     }
 
     @PostMapping("/{reviewId}/check")
     public ResponseEntity<?> checkReview (@PathVariable("reviewId") Long reviewId, @Valid @RequestBody ReviewRequestDTO.Check checkReview) {
-        return Logging.PrintLogAndReturnResponseEntity(reviewService.checkReview(reviewId, checkReview));
+        return reviewService.checkReview(reviewId, checkReview);
     }
 
     @PostMapping("/list")
     public ResponseEntity<?> listReview (@Valid @RequestBody ReviewRequestDTO.ListRequest listReview){
-        return Logging.PrintLogAndReturnResponseEntity(reviewService.reviewList(listReview));
+        return reviewService.reviewList(listReview);
     }
 }
