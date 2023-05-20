6225c1d09549e8545f56b795d2b6d382e6709408
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2023-05-21 01:57:57 +0900

    table name typo, delete time error fix

--- src/main/java/springbeam/susukgwan/schedule/TimeRepository.java
+++ src/main/java/springbeam/susukgwan/schedule/TimeRepository.java
@@ -1,8 +1,19 @@
 package springbeam.susukgwan.schedule;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface TimeRepository extends JpaRepository<Time, Long> {
+    @Modifying
+    @Transactional
+    @Query(
+            value= "DELETE FROM susukgwan.time WHERE tutoring_id = :tutoringId",
+            nativeQuery = true
+    )
+    void deleteByTutoringId(@Param(value="tutoringId") Long tutoringId);
 }
