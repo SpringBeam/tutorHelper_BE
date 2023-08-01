e44b54b90b7e3f9b720ebe6178a5d16e76e74412
강현석 <hs@ganghyeonseog-ui-MacBookAir.local>
2023-08-02 00:41:39 +0900

    approve invitation response fix, send tutoring id as the response

---
+++ src/main/java/springbeam/susukgwan/tutoring/dto/ApproveResponseDTO.java
@@ -0,0 +1,12 @@
+package springbeam.susukgwan.tutoring.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApproveResponseDTO {
+    private Long tutoringId;
+}
