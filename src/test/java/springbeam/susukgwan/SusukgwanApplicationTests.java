198c5bf77b57ecde23085db248d64f835b50dfad
grindincodes <hs@ganghyeonseog-ui-MacBookAir.local>
2023-11-08 17:38:58 +0900

    alarm set to true (At first save)

--- src/test/java/springbeam/susukgwan/SusukgwanApplicationTests.java
+++ src/test/java/springbeam/susukgwan/SusukgwanApplicationTests.java
@@ -44,7 +44,8 @@ class SusukgwanApplicationTests {
 
 	@Test
 	void sendNotification() {
-		PushRequest pushRequest = PushRequest.builder().title("수업시간 변경").topic("replace").body("수업의 ~").token("").build();
+		PushRequest pushRequest = PushRequest.builder().title("아아 ㅋㅋㅋㅋㅋ ").topic("된다").body("하하하")
+				.token("c5DcNXdrRSC5vHea45d6UO:APA91bGTNOkkipQKP8iyGTC9nsSjo1WqPw86wt0Y5A5hJqUigNV45FyybOQG9r2kE6dqJ6i2hlC-294DN6KF48uy2-RtDunGRC7IT7ckG3GjuskmYNpRulurxLtqR_RVNR60qRx_vK9z").build();
 		try {
 			fcmService.sendMessageToToken(pushRequest);
 			return;
